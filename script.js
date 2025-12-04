1ceaa728a7b30fa064b8f15bf9e04bd1605117dc
jwang2706 <wangjiale803@gmail.com>
2025-12-04 11:15:14 +0100

    Update script.js

--- script.js
+++ script.js
@@ -3,4 +3,14 @@ console.log("Inizio esecuzione");
 let numero = document.getElementById("InNumber").value;
     alert(numero);
  }
+if (numero >= 0 && numero <= 255){
+ document.getElementById("outText").innerHTML= "Hai inserito il" + "numero"+ numero;
+}
+else {
+ document.getElementById("error").innerHTML= "Hai numero non è corretto";
+}
+function reset(){
+ document.getElementById("inNumber").value = "";
+}
+
 
